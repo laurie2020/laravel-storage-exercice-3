a05ee861a67004e13088341dac87be03cc4af434
Laurie <laurieiula2016@gmail.com>
2021-05-17 13:10:23 +0200

    nav + structure home + structure backoffice

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,20 @@
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href={{ route('home') }} id="home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" id="users">Users</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" id="caracteristiques">Caracteristiques</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#" id="galeries">Galeries</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#" id="portfolios">Portfolios</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#" id="services">Services</a>
+    </li>
+</ul>
