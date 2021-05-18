8836319fbca0dde5fb9f87f290e492e53ceb110c
LeMufas2a <codage.mufasa@gmail.com>
2021-05-18 10:32:21 +0200

    show ok

--- resources/views/backoffice/caracteristique/show.blade.php
+++ resources/views/backoffice/caracteristique/show.blade.php
@@ -4,7 +4,7 @@
 @include('partials.nav')
 <section class="text-center mx-5 px-5">
     <h1 class="service-title text-center mt-3"><i class="fab {{ $caracteristique->icone }}"></i></h1>
-    <h2 class="text-center text-dark mt-3">{{ $caracteristique->titre }} </h2>
-    <h2 class="text-center text-dark mt-3">{{ $caracteristique->description }}</h2>
+    <h2 class="text-center text-dark mt-3">{{ $caracteristique->nom }}</h2>
+    <h2 class="text-center text-dark mt-3">{{ $caracteristique->chiffre }} </h2>
 </section>
 @endsection
